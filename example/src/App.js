3b740d3d8c834215b4aa4a31d3a0ac7aa50d2ec4
shauryabahuguna-tmpl <shaurya.b@tunica.tech>
2024-02-21 00:49:39 +0530

    initial commits for search bar

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-ai-search-bar'
+import { SearchBar } from 'react-ai-search-bar'
 import 'react-ai-search-bar/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <div className='container'>
+    <div>LOGO</div>
+    <ul className='list'>
+      <li>one</li>
+      <li>two</li>
+      <li>three</li>
+      <li>four</li>
+    </ul>
+    <SearchBar text="Create React Library Example 😄" placeholder="Enter your Query" />
+    <button>
+      Login
+    </button>
+  </div>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const SearchBar = ({ text, placeholder }) => {
+  const [input, setInput] = useState('')
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value)
+  }
+
+  return (
+    <div className={styles.container}>
+      <input
+        className={styles.input}
+        value={input}
+        onChange={handleInputChange}
+        placeholder={placeholder}
+      />
+      {input ? (
+        <div className={styles.searchOverlay}>
+          <div className={styles.searchResultBox}>HI</div>
+        </div>
+      ) : null}
+    </div>
+  )
 }
