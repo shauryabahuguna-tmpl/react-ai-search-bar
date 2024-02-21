e7ef37158ece1471e80912862d4c7de39e9d2796
shauryabahuguna-tmpl <shaurya.b@tunica.tech>
2024-02-21 19:06:15 +0530

    Search bar changes

--- example/src/App.js
+++ example/src/App.js
@@ -4,19 +4,25 @@ import { SearchBar } from 'react-ai-search-bar'
 import 'react-ai-search-bar/dist/index.css'
 
 const App = () => {
-  return <div className='container'>
-    <div>LOGO</div>
-    <ul className='list'>
-      <li>one</li>
-      <li>two</li>
-      <li>three</li>
-      <li>four</li>
-    </ul>
-    <SearchBar text="Create React Library Example 😄" placeholder="Enter your Query" />
-    <button>
-      Login
-    </button>
-  </div>
+  return (
+    <div className='container'>
+      <div className='navbar'>
+        <div>LOGO</div>
+        <ul className='list'>
+          <li>one</li>
+          <li>two</li>
+          <li>three</li>
+          <li>four</li>
+        </ul>
+        <SearchBar
+          text='Create React Library Example 😄'
+          // placeholder='Enter your Query'
+        />
+        <button>Login</button>
+      </div>
+      <div className='innerContainer'>Bottom</div>
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,26 +1,56 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './styles.module.css'
+import { Search, Send, X as Cross } from 'react-feather'
 
 export const SearchBar = ({ text, placeholder }) => {
   const [input, setInput] = useState('')
+  const [searched, setSearched] = useState(false)
 
   const handleInputChange = (e) => {
     setInput(e.target.value)
   }
 
+  const handleSendClick = (e) => {
+    setSearched(true)
+  }
+
+  useEffect(() => {
+    if (input === '') {
+      setSearched(false)
+    }
+  }, [input])
+
+  console.log(input)
+
   return (
     <div className={styles.container}>
-      <input
-        className={styles.input}
-        value={input}
-        onChange={handleInputChange}
-        placeholder={placeholder}
-      />
-      {input ? (
-        <div className={styles.searchOverlay}>
-          <div className={styles.searchResultBox}>HI</div>
+      <div className={styles.inputContainer}>
+        <Search className={styles.searchIcon} color='grey' size={18} />
+        <input
+          className={styles.input}
+          value={input}
+          onChange={handleInputChange}
+          placeholder={placeholder || 'Hi, How can I help you?'}
+        />
+        {input ? (
+          <Cross className={styles.close} color='grey' size={16} />
+        ) : null}
+        <Send
+          color={input ? 'green' : 'grey'}
+          size={16}
+          className={`${styles.sendIcon} ${input ? styles.iconActive : null}`}
+          onClick={input ? handleSendClick : null}
+        />
+      </div>
+      <div
+        className={`${styles.searchOverlay} ${
+          searched ? styles.slideDown : styles.slideUp
+        }`}
+      >
+        <div className={styles.searchResultBox}>
+          <div className={styles.innerSearch}>{input}</div>
         </div>
-      ) : null}
+      </div>
     </div>
   )
 }
