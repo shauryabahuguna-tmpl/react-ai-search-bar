098bf1adc1e4a63f1495f0b0f1946df0306adee3
shauryabahuguna-tmpl <shaurya.b@tunica.tech>
2025-01-28 10:19:41 +0530

    Version updated

--- src/index.js
+++ src/index.js
@@ -763,7 +763,7 @@ if (typeof window !== 'undefined') {
           ),
           // Load your styles.module.css
           loadStylesheet(
-            'https://cdn.jsdelivr.net/npm/react-ai-search-bar@1.0.4-beta.21/dist/index.umd.css'
+            'https://cdn.jsdelivr.net/npm/react-ai-search-bar@1.0.4-beta.22/dist/index.umd.css'
           )
         ])
       } catch (error) {
