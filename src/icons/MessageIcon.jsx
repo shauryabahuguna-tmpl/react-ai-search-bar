56908c1827238ee6d1232f13d001b8d29bbd6ce9
SujalTunica <sujal.v@tunica.tech>
2025-08-28 12:44:49 +0530

    form integartion

---
+++ src/icons/MessageIcon.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+const MessageIcon = (props) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width={24}
+    height={24}
+    fill='none'
+    stroke='currentColor'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    strokeWidth={2}
+    className='lucide lucide-message-square-icon lucide-message-square'
+    {...props}
+  >
+    <path d='M22 17a2 2 0 0 1-2 2H6.828a2 2 0 0 0-1.414.586l-2.202 2.202A.71.71 0 0 1 2 21.286V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2z' />
+  </svg>
+)
+export default MessageIcon
