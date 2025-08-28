d6100311182506df5f39b009bbb89fbf2781e3f8
Sahil-Bhaardwaj <sahil.bhaardwaj@ethinos.com>
2025-08-28 12:54:18 +0530

    Changed hardcoded tunica url to dynamically fetch website url

--- src/index.js
+++ src/index.js
@@ -226,8 +226,7 @@ const SearchBar = ({
   ]
 
   const baseUrl = 'https://api-qa.seekrs.ai'
-  // const Url = window?.location?.origin
-  const Url = 'https://www.tunica'
+  const Url = window?.location?.origin
   const currentPage = window?.location?.href
   const sessionCookie = Cookies.get('session')
   const sessionData = sessionCookie ? JSON.parse(sessionCookie) : null
