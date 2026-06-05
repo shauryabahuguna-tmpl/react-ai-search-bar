2c7e7cd2787e5120398a8d72515064f3c0b14ada
Sahil-Bhaardwaj <sahil.bhaardwaj@ethinos.com>
2026-06-05 15:23:12 +0530

    fix: Update URL assignment in SearchBar component for correct origin

--- src/index.js
+++ src/index.js
@@ -224,7 +224,7 @@ const SearchBar = ({
   ]
 
   const baseUrl = 'https://api.seekrs.ai'
-  const Url = window?.location?.origin
+  const Url = 'https://www.thefirstgroup.com/en/'
   const currentPage = window?.location?.href
   const sessionCookie = Cookies.get('session')
   const sessionData = sessionCookie ? JSON.parse(sessionCookie) : null
